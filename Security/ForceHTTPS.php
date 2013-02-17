84cd4c22c2db5f5a35c473dd0fec02eeab58d9f6
Rich <richhildebrand@gmail.com>
2013-02-17 15:49:51 -0500

    reordered HTTPS check

--- Security/ForceHTTPS.php
+++ Security/ForceHTTPS.php
@@ -1,4 +1,4 @@
 <?php
-	if ( $_SERVER["HTTPS"] != "on" && $_SERVER['HTTP_HOST'] == 'webdev.cs.kent.edu') {
-		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+	if ( $_SERVER['HTTP_HOST'] == 'webdev.cs.kent.edu' && $_SERVER['HTTPS'] != 'on') {
+		header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 	}
