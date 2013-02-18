e2b8abfdc664db24eddc633c9f7523466e54e5c9
Rich <richhildebrand@gmail.com>
2013-02-17 22:04:28 -0500

    removed flagged attributes from register.php

--- Account/Register.php
+++ Account/Register.php
@@ -11,17 +11,17 @@
     </head>
     <body>
         <h1>Register for Paul's Pizza Palace</h1>
-        <form method="post" action="">
-            <label for="email">Enter your email address</label>
+        <form method="post" >
+            <label >Enter your email address</label>
             <input type="email" required autofocus name="email" />
             <span class="error"> <?php print($errorResult) ?> </span>
             
-            <label for="password">Create a password between 5 and 250 characters</label>
+            <label >Create a password between 5 and 250 characters</label>
             <input type="password" pattern=".{5,250}" required 
                    name="password" id="password" onkeyup="confirmPasswordsMatch()"/>
             <span id="password-warning-one" class="error hide"> Your Passwords do not match </span>
 
-            <label for="confirm-password">Confirm your password</label>
+            <label >Confirm your password</label>
             <input type="password" pattern=".{5,250}" required 
                    name="confirm-password" id="confirm-password" onkeyup="confirmPasswordsMatch()"/>
             <span id="password-warning-two" class="error hide"> Your Passwords do not match </span>
