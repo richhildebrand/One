5a4f73765e4ea742a07c30709860bb8f470e5780
Rich <richhildebrand@gmail.com>
2013-02-17 21:54:46 -0500

    removed flagged attributes

--- Account/Login.php
+++ Account/Login.php
@@ -11,12 +11,12 @@
     </head>
     <body>
         <h1>Login to Paul's Pizza Palace</h1>
-        <form method="post" action="">
-            <label for="email">Email</label>
+        <form method="post">
+            <label >Email</label>
             <input type="email" maxlength="250" required autofocus name="email" />
             <span class="error"> <?php print($errorResult) ?> </span>
 
-            <label for="password">Password</label>
+            <label >Password</label>
             <input type="password" maxlength="250" required name="password" />
 
             <button name="Login">Login</button>
