fcc6a3bdfba7cdb65a2bf81a0513b1319a362e40
Rich <richhildebrand@gmail.com>
2013-02-18 18:54:19 -0500

    refactored logger

--- BusinessLogic/Logger.php
+++ BusinessLogic/Logger.php
@@ -1,68 +1,44 @@
 <?php
 
-/**
- * Logging class:
- * - contains lfile, lwrite and lclose public methods
- * - lfile sets path and name of log file
- * - lwrite writes message to the log file (and implicitly opens log file)
- * - lclose closes log file
- * - first call of lwrite method will open log file implicitly
- * - message is written with the following format: [d/M/Y:H:i:s] (script name) message
- */
 class Logger {
-    // declare log file and file pointer as private properties
-    private $log_file, $fp;
+    private $_logFileName
+    private $_fileStream;
 
     public function __construct() {
-        $this->log_file = "log.txt";
+        $this->_logFileName = "log.txt";
     }
 
-    public function writeArray($array)
+    private function writeArray($array)
     {
         foreach ($array as $key => $value)
         {
             $this->write("key = " . $key . " value = " . $value);
-            if(is_array($value)){ //If $value is an array, print it as well!
+            if(is_array($value)){ 
                 $this->writeArray($value);
             }  
         } 
     }
 
-
-    // write message to the log file
     public function write($message) {
-        // if file pointer doesn't exist, then open log file
-        if (!is_resource($this->fp)) {
-            $this->lopen();
+        if (!is_resource($this->_fileStream)) {
+            $this->open();
         }
-        // define script name
+
         $script_name = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-        // define current time and suppress E_WARNING if using the system TZ settings
-        // (don't forget to set the INI setting date.timezone)
         $time = @date('[d/M/Y:H:i:s]');
-        // write current time, script name and message to the log file
-        fwrite($this->fp, "$time ($script_name) $message" . PHP_EOL);
+        fwrite($this->_fileStream, "$time ($script_name) $message" . PHP_EOL);
+
+        if (is_array($message) 
+        {
+            $this->writeArray($message);
+        }
     }
 
-    // close log file (it's always a good idea to close a file when you're done with it)
-    public function lclose() {
-        fclose($this->fp);
+    public function close() {
+        fclose($this->_fileStream);
     }
     
-    // open log file (private method)
-    private function lopen() {
-        // in case of Windows set default log file
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $log_file_default = 'c:/php/logfile.txt';
-        }
-        // set default log file for Linux and other systems
-        else {
-            $log_file_default = '/tmp/logfile.txt';
-        }
-        // define log file from lfile method or use previously set default
-        $lfile = $this->log_file ? $this->log_file : $log_file_default;
-        // open log file for writing only and place file pointer at the end of the file
-        // (if the file does not exist, try to create it)
-        $this->fp = fopen($lfile, 'a') or exit("Can't open $lfile!");
+    private function open( $fileName ) {
+        $this->_fileStream = fopen($this->_logFileName, 'a') or exit("Can't open file " . $this->_logFileName);
     }
 }
