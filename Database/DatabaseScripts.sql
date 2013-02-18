9da0cca7e824efaaf589dee952e10c6adc96ba56
Rich <richhildebrand@gmail.com>
2013-02-17 20:25:47 -0500

    updated script

--- Database/DatabaseScripts.sql
+++ Database/DatabaseScripts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
 		userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         useremail VARCHAR(250) NOT NULL,
-        userpassword VARCHAR(250) NOT NULL,
+        userpassword VARCHAR(250) NOT NULL
        );
